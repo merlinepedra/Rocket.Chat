504622befb8a5e8f176cd27b1926fa1733316ee9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-06-27 19:27:24 -0300

    qtt server

--- server/main.js
+++ server/main.js
@@ -1,1 +1,9 @@
 import '/imports/startup/server';
+import { init, connect } from 'rocket.chat.mqtt';
+
+Meteor.startup(function() {
+	const options = init({
+		Subscriptions: RocketChat.models.Subscriptions.model.rawCollection()
+	});
+	connect(options);
+});
