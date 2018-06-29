6b9be96e1cfc631a426dc9101fd9d28e09042b25
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-06-29 11:34:35 -0300

    added non redis mq

--- packages/rocketchat-lib/server/lib/mq.js
+++ packages/rocketchat-lib/server/lib/mq.js
@@ -1,6 +1,4 @@
+import mq from 'mqemitter';
 import redisMq from 'mqemitter-redis';
 
-export default process.env.REDIS &&
-	redisMq({
-		host: process.env.REDIS
-	});
+export default process.env.REDIS ? redisMq({ host: process.env.REDIS }) : mq();
