942acd5543a90ea737f3d5c9a342b52e4bec7e0a
Diego Sampaio <chinello@gmail.com>
2018-07-18 17:52:59 -0300

    Fix bugs

--- packages/rocketchat-lib/server/functions/checkUsernameAvailability.js
+++ packages/rocketchat-lib/server/functions/checkUsernameAvailability.js
@@ -8,7 +8,7 @@ const trimUsername = username => username.trim();
 const toRegExp = username => new RegExp(`^${s.escapeRegExp(username)}$`, 'i');
 
 RocketChat.settings.get('Accounts_BlockedUsernameList', (key, value) => {
-	usernameBlackList = value.split(',').map(value).map(toRegExp);
+	usernameBlackList = value.split(',').map(trimUsername).map(toRegExp);
 });
 
 const usernameIsBlocked = (username, usernameBlackList) => usernameBlackList.length
