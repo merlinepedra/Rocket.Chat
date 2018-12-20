cd064935c16bc39565f60d064542faec75aeefb5
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-12-20 18:30:08 -0200

    fix create room

--- imports/rocketchat-lib/server/actions/createRoom.js
+++ imports/rocketchat-lib/server/actions/createRoom.js
@@ -1,6 +1,6 @@
 export default {
-	async createRoom(ctx) {
-		const { type, members, readOnly, customFields, extraData, uid } = ctx.params;
+	createRoom(ctx) {
+		const { name, type, members, readOnly, customFields, extraData, uid } = ctx.params;
 		const { username } = RocketChat.models.Users.findOneById(uid);
 		return RocketChat.Services.createRoom(type, name, username, members, readOnly, { customFields, ...extraData });
 	},
