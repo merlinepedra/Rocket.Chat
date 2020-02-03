76072695d9cf4dfd43fb9085a188ff5f59464cf0
Renato Becker <renato.augusto.becker@gmail.com>
2020-02-03 11:02:03 -0300

    [NEW] Check the Omnichannel service status per Department (#16425)
    * Allow to check the service status per department.
    
    * update livechat bridge method isOnline
    
    Co-authored-by: Shiqi Mei <shiqi.mei@lolimay.cn>

--- app/livechat/server/lib/QueueManager.js
+++ app/livechat/server/lib/QueueManager.js
@@ -10,10 +10,6 @@ import { Livechat } from './Livechat';
 
 export const QueueManager = {
 	async requestRoom({ guest, message, roomInfo, agent }) {
-		if (!Livechat.online()) {
-			throw new Meteor.Error('no-agent-online', 'Sorry, no online agents');
-		}
-
 		check(message, Match.ObjectIncluding({
 			rid: String,
 		}));
@@ -23,6 +19,10 @@ export const QueueManager = {
 			status: Match.Maybe(String),
 			department: Match.Maybe(String),
 		}));
+
+		if (!Livechat.online(guest.department)) {
+			throw new Meteor.Error('no-agent-online', 'Sorry, no online agents');
+		}
 
 		const { rid } = message;
 		const name = (roomInfo && roomInfo.fname) || guest.name || guest.username;
