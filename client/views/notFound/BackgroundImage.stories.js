40bb83b17a658ebfe15181594eceb846038525c2
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-04 22:22:09 -0300

    Add light mode to BackgroundImage

--- client/views/notFound/BackgroundImage.stories.js
+++ client/views/notFound/BackgroundImage.stories.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 import BackgroundImage from './BackgroundImage';
+import { useAutoToggle } from '../../../.storybook/hooks';
 
 export default {
 	title: 'views/notFound/BackgroundImage',
 	component: BackgroundImage,
 };
 
-export const Default = () => <BackgroundImage />;
+export const Default = () =>
+	<BackgroundImage />;
+
+export const Dark = () =>
+	<BackgroundImage dark />;
+
+export const Light = () =>
+	<BackgroundImage dark={false} />;
+
+export const Transitioning = () =>
+	<BackgroundImage dark={useAutoToggle(true)} />;
