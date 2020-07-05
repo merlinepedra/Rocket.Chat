09b2285d19d7e183826601c064fe7cda91c5b58b
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-04 22:02:30 -0300

    Update Fuselage type declarations

--- client/fuselage.d.ts
+++ client/fuselage.d.ts
@@ -1,3 +1,9 @@
+declare module '@rocket.chat/css-in-js' {
+	type cssFn = (...args: unknown[]) => string;
+	export function css(strings: TemplateStringsArray, ...values: unknown[]): cssFn;
+	export function toClassName(fn: cssFn): string | undefined;
+}
+
 declare module '@rocket.chat/fuselage-hooks' {
 	export const useDebouncedCallback: (fn: (...args: any[]) => any, ms: number, deps: any[]) => (...args: any[]) => any;
 	export const useMutableCallback: (fn: (...args: any[]) => any) => (...args: any[]) => any;
