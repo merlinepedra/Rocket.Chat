a8803d2479fa0dbb9ebee89ba5f844a038eea9e7
Diego Sampaio <chinello@gmail.com>
2021-07-02 17:19:50 -0300

    More definitions

--- app/oembed/server/convertOembedToUiKit.ts
+++ app/oembed/server/convertOembedToUiKit.ts
@@ -6,20 +6,32 @@ type OembedUrlLegacy = {
 }
 
 type OembedMeta = Partial<{
-	siteName: string;
-	siteUrl: string;
-	title: string;
-	description: string;
+	siteName: string; // nome
+	siteUrl: string; // parsedUrl.host
+	// siteColor: string; // ogColor
+	title: string; // ogTitle
+	description: string; // ogDescription
+	// author: string; // ??
 	image: {
-		url: string;
+		preview?: string; // base64 low res preview
+		src: string;
 		dimensions: {
 			width?: number;
 			height?: number;
 		};
 	};
 	url: string;
-	type: string;
+	type: string; // OembedType
 }>
+
+/*
+OembedType
+	external - preview com jump to
+	embedded - play and preview com jump to
+	video - attachment
+	image - attachment
+	audio - attachment
+*/
 
 export const normalizeMeta = ({ url, meta }: OembedUrlLegacy): OembedMeta => {
 	const image = meta.ogImage || meta.twitterImage || meta.msapplicationTileImage || meta.oembedThumbnailUrl;
@@ -29,29 +41,35 @@ export const normalizeMeta = ({ url, meta }: OembedUrlLegacy): OembedMeta => {
 		title: meta.ogTitle || meta.twitterTitle || meta.title || meta.pageTitle,
 		description: meta.ogDescription || meta.twitterDescription || meta.description,
 		...image && {
-			image: { url: image,
+			image: {
+				url: image,
 				dimensions: {
 					...meta.ogImageHeight && { height: meta.ogImageHeight },
 					...meta.ogImageHeight && { width: meta.ogImageWidth },
-				} },
+				},
+			},
 		},
 		url,
 		type: meta.ogType,
 	}).filter(([, value]) => value));
 };
 
-export const convertOembedToUiKit = (urls: OembedUrlLegacy[]): PreviewBlock<VisibilityTypesInternal>[] => urls.filter(({ meta }) => Boolean(meta)).map(normalizeMeta).map(({ title, description, url, image, type }) => ({
-	type: 'preview',
-	title: {
-		type: 'plain_text',
-		text: title,
-	},
-	description: {
-		type: 'plain_text',
-		text: description,
-	},
-	...url && { externalUrl: url },
-	...image && {
-		[image.dimensions.height && image.dimensions ? 'preview' : 'thumb']: image,
-	},
-}));
+export const convertOembedToUiKit = (urls: OembedUrlLegacy[]): PreviewBlock<VisibilityTypesInternal>[] =>
+	urls
+		.filter(({ meta }) => Boolean(meta))
+		.map(normalizeMeta)
+		.map(({ title, description, url, image, type }) => ({
+			type: 'preview',
+			title: {
+				type: 'plain_text',
+				text: title,
+			},
+			description: {
+				type: 'plain_text',
+				text: description,
+			},
+			...url && { externalUrl: url },
+			...image && {
+				[image.dimensions.height && image.dimensions ? 'preview' : 'thumb']: image,
+			},
+		}));
