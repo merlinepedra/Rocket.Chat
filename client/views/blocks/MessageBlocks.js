03b2f6457cbbf2269a99bb34924d2a038ca896d7
Guilherme Gazzo <guilherme@gazzo.xyz>
2021-07-07 14:43:33 -0300

    MD MB

--- client/views/blocks/MessageBlocks.js
+++ client/views/blocks/MessageBlocks.js
@@ -1,10 +1,22 @@
 import { UIKitIncomingInteractionContainerType } from '@rocket.chat/apps-engine/definition/uikit/UIKitIncomingInteractionContainer';
-import { UiKitMessage, UiKitComponent, kitContext } from '@rocket.chat/fuselage-ui-kit';
 import { MessageBlock } from '@rocket.chat/fuselage';
+import {
+	UiKitMessage,
+	UiKitComponent,
+	kitContext,
+	messageParser,
+} from '@rocket.chat/fuselage-ui-kit';
 import React, { useRef, useEffect } from 'react';
 
 import * as ActionManager from '../../../app/ui-message/client/ActionManager';
+import { renderMessageBody } from '../../lib/renderMessageBody';
 import './textParsers';
+
+// TODO: move this to fuselage-ui-kit itself
+const mrkdwn = ({ text } = {}) =>
+	text && <span dangerouslySetInnerHTML={{ __html: renderMessageBody({ msg: text }) }} />;
+
+messageParser.mrkdwn = mrkdwn;
 
 function MessageBlocks({ mid: _mid, rid, blocks, appId }) {
 	const context = {
