00ebaea51cbb78b61fcbd0433c6d5f9f121210de
Leonardo Ostjen Couto <leonardoostjen@gmail.com>
2021-10-20 11:55:47 -0300

    updated migration

---
+++ server/startup/migrations/v241.ts
@@ -0,0 +1,43 @@
+import { addMigration } from '../../lib/migrations';
+import { Uploads } from '../../../app/models/server';
+
+
+addMigration({
+	version: 241,
+	up() {
+		const images = Uploads.model.rawCollection().find({ type: { $regex: '^(image/).+' } });
+		const getAspectRatio = (image: Record<string, any>) => image.identify.size.width / image.identify.size.height;
+
+		// proportion rate error margin 7%
+		const epsilon = 0.07;
+
+		// 2 seconds
+		const thumbCreationTimeout = 2000;
+
+		images.forEach(
+			(image: Record<string, any>) => {
+				const possibleThumbnails = Uploads.model.rawCollection().find({ name: `thumb-${ image.name }` });
+				if (!possibleThumbnails.count()) { return; }
+
+				possibleThumbnails.forEach(
+					(thumb: Record<string, any>) => {
+						const aspectRatioProportion = getAspectRatio(image) / getAspectRatio(thumb);
+						const uploadedAtDelta = thumb.uploadedAt - image.uploadedAt;
+
+						if (uploadedAtDelta < thumbCreationTimeout && (Math.abs(1 - aspectRatioProportion) < epsilon)) {
+							Uploads.model.rawCollection().update(
+								{
+									_id: thumb._id,
+								},
+								{
+									$set: {
+										unlisted: true,
+									},
+								});
+						}
+					},
+				);
+			},
+		);
+	},
+});
